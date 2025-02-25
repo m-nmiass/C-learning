67b480753af820894eb43f28a0fb869f8f5772eb
m-nmiass <mhamednmiass333@gmail.com>
2025-02-25 05:52:00 +0100

    done

--- Strings/project3/READme.md
+++ Strings/project3/READme.md
@@ -1,5 +1,5 @@
 # ft-strncpy
 
 ## Overview
-char * ft-strncpy(char * s1, const char * s2, int n);
-This function will copy n caracters from s2 to s1
+char * ft-strncpy (char * s1, char * s2, size-t n)
+this function will copy s2 to s1 up to n times if n is bigger then s2 then the rest will be null characters
--- Strings/project3/strncpy.c
+++ Strings/project3/strncpy.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
-
-char *ft_strncpy(char *s1, const char *s2, int n)
+size_t *ft_strncpy(char *s1, const char *s2, size_t n)
 {
-	if (!s1 || !s2 || !n)
-		return s1 ;
+	if (!s1 || !s2)
+		return NULL;
+	if (n == 0)
+		return s1;
 
 	size_t i = 0 ;
-
-	while (i < n && s2[i] != '\0')
+	
+	while (s2[i] != '\0' && i < n)
 	{
-		s1[i] = s2[i] ;
+		s1[i] = s2[i];
 		i++;
 	}
-
 
 	while (i < n)
 	{
 		s1[i] = '\0';
 		i++;
 	}
-
-	return s1 ;
+	return s1;
 }
-
