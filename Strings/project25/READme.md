d4533b5ae408759785a2232a2f8984bb28c1c114
m-nmiass <mhamednmiass333@gmail.com>
2025-03-21 15:06:53 +0100

    done

---
+++ Strings/project25/READme.md
@@ -0,0 +1,4 @@
+# Name
+
+## Overview
+This program reprent a name entered by the user in a form (last name, first character of the first name)
---
+++ Strings/project25/name.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+#define SIZE 100
+
+void reverse_name(char *name);
+void read_name(char *name);
+
+int main()
+{
+	char name[SIZE];
+
+	printf("Enter the name : ");
+	read_name(name);
+
+	reverse_name(name);
+
+	return 0;
+}
+
+void reverse_name(char *name)
+{
+	char ch = name[0];
+	int i = 1;
+	for(; name[i] != ' ' && name[i] != '\t'; i++);
+       	for(; name[i] == ' ' || name[i] == '\t'; i++);
+	for(; name[i] != '\0'; i++)
+		printf ("%c", name[i]);
+	printf(", %c", ch);
+}
+
+void read_name(char *name)
+{
+	char ch;
+	int i = 0;
+
+	while((ch = getchar()) != '\n') {
+		if(i < SIZE)
+			name[i++] = ch;
+	}
+
+	name[i] = '\0';
+}
