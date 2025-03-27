5aea3f5192dcd9192f4843df7355598b7d81eb72
m-nmiass <mhamednmiass333@gmail.com>
2025-03-27 14:56:31 +0100

    done

---
+++ Writting_Large_Programs/project1/main.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<operations.h>
+
+int main()
+{
+	int x, y ;
+	int operation;
+
+	printf("Enter the first number : ");
+	scanf("%d", &x);
+
+	printf("Enter the second number : ");
+	scanf("%d", &y);
+
+	printf("1 = add, 2 = sub, 3 = mult, 4 = div, 5 = mod\n");
+	printf("Enter the operation : ");
+	scanf("%d", &operation);
+
+	if(operation == 1)
+		printf("%d + %d = %d", x, y, add(x, y));
+	else if(operation == 2)
+		printf("%d - %d = %d", x, y, sub(x, y));
+	else if(operation == 3)
+		printf("%d * %d = %d", x, y, mul(x, y));
+	else if (operation == 4)
+		printf("%d / %d = %d", x, y, div(x, y));
+	else if (operation == 5)
+		printf("%d % %d = %d", x, y, dev(x, y));
+	else
+		printf("Invalid operation");
+}
---
+++ Writting_Large_Programs/project1/opperations.h
@@ -0,0 +1,5 @@
+int add(int x, int y);
+int sub(int x, int y);
+int dev(int x, int y);
+int mul(int x, int y);
+int mod(int x, int y);
