73c8ebf4716e844a8bc0b4428e2b615dd1289c24
m-nmiass <mhamednmiass333@gmail.com>
2025-03-27 15:42:05 +0100

    done

---
+++ Writting_Large_Programs/project2/array.c
@@ -0,0 +1,13 @@
+int arr[] = {1, 2, 3, 4, 5};
+
+void print_array()
+{
+	for(int i = 0; i < 5; i++)
+		printf("%d ", arr[i]);
+}
+
+void modify_array(int j)
+{
+	for(int i = 0; i < 5; i++)
+		arr[i] *= j;
+}
---
+++ Writting_Large_Programs/project2/array.h
@@ -0,0 +1,3 @@
+extern int arr[];
+void print_array();
+void modify_array();
