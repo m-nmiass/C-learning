e234f90f25406c45ab636c9c58550d10db25edcf
m-nmiass <mhamednmiass333@gmail.com>
2025-03-27 15:44:20 +0100

    done

---
+++ Writting_Large_Programs/project2/main.c
@@ -0,0 +1,11 @@
+#include<array.h>
+#include<stdio.h>
+
+int main()
+{
+	printf("Array befor modidfication : ");
+	print_array();
+	modify_array(2);
+	printf("Array after modification : ");
+	print_array();
+}
