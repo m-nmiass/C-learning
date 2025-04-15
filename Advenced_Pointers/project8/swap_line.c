ed63c38df4eaa9dac974f586be987f940bfc1070
m-nmiass <mhamednmiass333@gmail.com>
2025-04-15 14:26:36 +0200

    done

---
+++ Advenced_Pointers/project8/swap_line.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	char c;
+	int size;
+	char *p;
+
+	printf("Enter the string size : ");
+	scanf("%d", &size);
+	getchar();
+
+	p = (char *)malloc(size * sizeof(char));
+
+	printf("Enter the string :");
+
+	for(int i = 0; i < size; i++)
+		p[i] = getchar();
+
+	printf("The reverse strng is : ");
+	for(int i = size - 1; i >= 0; i--)
+		printf("%c", p[i]);
+
+	return 0;
+}
