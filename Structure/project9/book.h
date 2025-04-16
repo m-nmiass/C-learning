ad91383b62756fc806e894bb2cbd4cd452e36c74
m-nmiass <mhamednmiass333@gmail.com>
2025-04-16 20:44:06 +0200

    done

---
+++ Structure/project9/book.h
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+#define MAX_BOOKS 100
+#define MAX_NAME 50
+
+typedef struct {
+	char title[MAX_NAME + 1];
+	char author[MAX_NAME + 1];
+	int book_pages;
+}book;
+
+extern book info[MAX_BOOKS];
+
+void read_line();
+void clear_cach();
+void add_book(int *max);
+void delete_book(int *max);
+void print_book(int i);
+void print_books(int max);
+void read_book(int *max);
